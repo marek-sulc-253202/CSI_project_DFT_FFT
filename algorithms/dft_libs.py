17ea79cf9db2591397ec17b3d88f959b9aec5b15
Marek šulc <253202@vutbr.cz>
2026-03-19 16:38:06 +0100

    Hotové komentáře dft_libs.py

--- algorithms/dft_libs.py
+++ algorithms/dft_libs.py
@@ -1,9 +1,14 @@
 from math import pi
 from cmath import exp
 
+# Funkce pro výpočet DFT za použití funkcí z knihoven math a cmath
 def calculate_dft(x):
     N = len(x)
-    X = [0.0j] * N
+    X = [0.0j] * N # připravení výstupní sekvence vzorků a naplnění ji komplexními nulami
+
+    # Algoritmus pro výpočet DFT, v podstatě SUMA v cyklech
+    # Vnější cyklus (k) postupně prochází frekvence a vnitřní cyklus (n) prochází signál v čase
+    # Kvůli těmto dvou vnořeným cykům to má tak vysokou výpočetní složitost
     for k in range(N):
         for n in range(N):
             X[k] += x[n] * exp(-2j * pi * k * n / N)
