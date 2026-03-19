46832612cb072e84374f2940859cc566a6183c47
Marek šulc <253202@vutbr.cz>
2026-03-19 16:37:43 +0100

    Hotové komentáře dft.py

--- algorithms/dft.py
+++ algorithms/dft.py
@@ -1,8 +1,13 @@
-from algorithms import mathfce
+from algorithms import mathfce # import implementovaných funkcí z mathfce.py
 
+# Funkce pro výpočet DFT
 def calculate_dft(x):
     N = len(x)
-    X = [0.0j] * N
+    X = [0.0j] * N # připravení výstupní sekvence vzorků a naplnění ji komplexními nulami
+
+    # Algoritmus pro výpočet DFT, v podstatě SUMA v cyklech
+    # Vnější cyklus (k) postupně prochází frekvence a vnitřní cyklus (n) prochází signál v čase
+    # Kvůli těmto dvou vnořeným cykům to má tak vysokou výpočetní složitost
     for k in range(N):
         for n in range(N):
             X[k] += x[n] * mathfce.complex_exponential(-2 * mathfce.pi * k * n / N)
