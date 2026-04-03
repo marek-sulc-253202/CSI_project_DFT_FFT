c8a710f5821b7a278cc0f48575948613de3fa1d9
Marek šulc <253202@vutbr.cz>
2026-04-03 22:14:21 +0200

    optimalizace dft_libs.py

--- algorithms/dft_libs.py
+++ algorithms/dft_libs.py
@@ -6,10 +6,15 @@ def calculate_dft(x):
     N = len(x)
     X = [0.0j] * N # připravení výstupní sekvence vzorků a naplnění ji komplexními nulami
 
+    # Spočítáme Taylorovy řady pouze N-krát, kdyby to bylo ve dvou nořených cyklech tak se to bude počítat N^2-krát.
+    W = [exp(-2j * pi * i /N) for i in range(N)]
+
     # Algoritmus pro výpočet DFT, v podstatě SUMA v cyklech
     # Vnější cyklus (k) postupně prochází frekvence a vnitřní cyklus (n) prochází signál v čase
     # Kvůli těmto dvou vnořeným cykům to má tak vysokou výpočetní složitost
     for k in range(N):
         for n in range(N):
-            X[k] += x[n] * exp(-2j * pi * k * n / N)
+            # Pomocí periodicity úhlů vždy dostanem za pomocí modula správný index požadovaného úhlu.
+            index = (k * n) % N
+            X[k] += x[n] * W[index]
     return X
